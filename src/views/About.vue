675853064e0e66c082160d0bc38c659739cdaa48
rosoks <rostyslav.oksenchuk@gmail.com>
2020-12-03 22:44:33 +0200

    Created menububble

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div>
+      <menu-bubble />
+    </div>
   </div>
 </template>
+
+<script>
+import MenuBubble from "@/components/MenuBubble.vue";
+
+export default {
+  name: "About",
+  components: {
+    MenuBubble
+  }
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <menu-bubble />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import MenuBubble from "../components/MenuBubble.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    MenuBubble
   }
 };
 </script>
